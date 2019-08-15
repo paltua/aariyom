ee38d97037ead55d96dbe6cb7545288eb1ebf605
Ranajit <paltua@gmail.com>
2019-08-15 22:44:20 +0530

    added component

--- src/app/admin/activity/add-edit/add-edit.component.ts
+++ src/app/admin/activity/add-edit/add-edit.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { routerTransition } from '../../../router.animations';
 
 @Component({
   selector: 'app-add-edit',
   templateUrl: './add-edit.component.html',
-  styleUrls: ['./add-edit.component.scss']
+  styleUrls: ['./add-edit.component.scss'],
+  animations: [routerTransition()]
 })
 export class AddEditComponent implements OnInit {
 
--- src/app/admin/event/listing/listing.component.ts
+++ src/app/admin/event/listing/listing.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { routerTransition } from '../../../router.animations';
+
 
 @Component({
   selector: 'app-listing',
   templateUrl: './listing.component.html',
-  styleUrls: ['./listing.component.scss']
+  styleUrls: ['./listing.component.scss'],
+  animations: [routerTransition()]
 })
 export class ListingComponent implements OnInit {
 
