85a83c774b3954152bbdcb5c121ca2d5b42e77ef
Ranajit <paltua@gmail.com>
2019-08-24 00:00:43 +0530

    added

--- src/app/admin/event/add-edit/add-edit.component.ts
+++ src/app/admin/event/add-edit/add-edit.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../../router.animations';
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-
+import { NgbTimeStruct } from '@ng-bootstrap/ng-bootstrap';
 
 
 
@@ -15,12 +15,32 @@ export class AddEditComponent implements OnInit {
 
   public editor = ClassicEditor;
   public editorData = '';
-  public start_date = '';
-  public model: any;
-  constructor(
-  ) { }
+  defaultTime = { hour: 13, minute: 30 };
+  meridianTime = { hour: 13, minute: 30 };
+  meridian = true;
+
+  SecondsTime: NgbTimeStruct = { hour: 13, minute: 30, second: 30 };
+  seconds = true;
+
+  customTime: NgbTimeStruct = { hour: 13, minute: 30, second: 0 };
+  hourStep = 1;
+  minuteStep = 15;
+  secondStep = 30;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
+
+  toggleSeconds() {
+    this.seconds = !this.seconds;
+  }
+
+  toggleMeridian() {
+    this.meridian = !this.meridian;
+  }
+
+
 
 }
--- src/app/admin/event/event.module.ts
+++ src/app/admin/event/event.module.ts
@@ -2,21 +2,55 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { EventRoutingModule } from './event-routing.module';
 import { ListingComponent } from './listing/listing.component';
 import { AddEditComponent } from './add-edit/add-edit.component';
 
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import {
+  AlertComponent,
+  ButtonsComponent,
+  ModalComponent,
+  CollapseComponent,
+  DatePickerComponent,
+  DropdownComponent,
+  PaginationComponent,
+  PopOverComponent,
+  ProgressbarComponent,
+  TabsComponent,
+  RatingComponent,
+  TooltipComponent,
+  TimepickerComponent
+} from './../components';
 
 
 @NgModule({
-  declarations: [ListingComponent, AddEditComponent],
   imports: [
     CommonModule,
     EventRoutingModule,
     CKEditorModule,
     NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  declarations: [
+    ListingComponent,
+    AddEditComponent,
+    ButtonsComponent,
+    AlertComponent,
+    ModalComponent,
+    CollapseComponent,
+    DatePickerComponent,
+    DropdownComponent,
+    PaginationComponent,
+    PopOverComponent,
+    ProgressbarComponent,
+    TabsComponent,
+    RatingComponent,
+    TooltipComponent,
+    TimepickerComponent
   ]
 })
 export class EventModule { }
