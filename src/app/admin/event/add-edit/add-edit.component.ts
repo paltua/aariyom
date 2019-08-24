c00ecb0c7e6108325a5214960406d00729ccf4c2
Ranajit <paltua@gmail.com>
2019-08-24 21:01:49 +0530

    added event section

--- src/app/admin/event/add-edit/add-edit.component.ts
+++ src/app/admin/event/add-edit/add-edit.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../../router.animations';
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import { NgbTimeStruct } from '@ng-bootstrap/ng-bootstrap';
+import { NgbTimeStruct, NgbDatepickerConfig, NgbCalendar, NgbDate, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder, Validators } from '@angular/forms';
 
 
 
@@ -9,7 +10,8 @@ import { NgbTimeStruct } from '@ng-bootstrap/ng-bootstrap';
   selector: 'app-add-edit',
   templateUrl: './add-edit.component.html',
   styleUrls: ['./add-edit.component.scss'],
-  animations: [routerTransition()]
+  animations: [routerTransition()],
+  providers: [NgbDatepickerConfig] // add NgbDatepickerConfig to the component providers
 })
 export class AddEditComponent implements OnInit {
 
@@ -26,8 +28,21 @@ export class AddEditComponent implements OnInit {
   hourStep = 1;
   minuteStep = 15;
   secondStep = 30;
+  eventForm = this.fb.group({
+    event_title: ['', Validators.required],
+    event_short_desc: ['', Validators.required],
+    event_start_date: ['', Validators.required],
+    event_end_date: ['', Validators.required],
+    event_start_time: ['', Validators.required],
+    event_end_time: ['', Validators.required],
+    event_long_desc: [''],
+  });
+  constructor(
+    private fb: FormBuilder,
+    private config: NgbDatepickerConfig,
+    private calendar: NgbCalendar
+  ) {
 
-  constructor() {
   }
 
   ngOnInit() {
@@ -39,6 +54,17 @@ export class AddEditComponent implements OnInit {
 
   toggleMeridian() {
     this.meridian = !this.meridian;
+  }
+
+  setStartDateConfig() {
+    this.config.minDate = { year: 1900, month: 1, day: 1 };
+    this.config.maxDate = { year: 2099, month: 12, day: 31 };
+
+    // days that don't belong to current month are not visible
+    this.config.outsideDays = 'hidden';
+
+    // weekends are disabled
+    // this.config.markDisabled = (date: NgbDate) => this.calendar.getToday(date) ==;
   }
 
 
