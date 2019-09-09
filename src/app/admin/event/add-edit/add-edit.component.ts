43fc30d54d5f088bfd4be3e70a16ebdf64357490
Ranajit <paltua@gmail.com>
2019-09-09 22:50:08 +0530

    added form validation

--- src/app/admin/event/add-edit/add-edit.component.ts
+++ src/app/admin/event/add-edit/add-edit.component.ts
@@ -7,73 +7,84 @@ import { FormBuilder, Validators } from '@angular/forms';
 
 
 @Component({
-  selector: 'app-add-edit',
-  templateUrl: './add-edit.component.html',
-  styleUrls: ['./add-edit.component.scss'],
-  animations: [routerTransition()],
-  providers: [NgbDatepickerConfig] // add NgbDatepickerConfig to the component providers
+	selector: 'app-add-edit',
+	templateUrl: './add-edit.component.html',
+	styleUrls: ['./add-edit.component.scss'],
+	animations: [routerTransition()],
+	providers: [NgbDatepickerConfig] // add NgbDatepickerConfig to the component providers
 })
 export class AddEditComponent implements OnInit {
 
-  public editor = ClassicEditor;
-  public editorData = '';
-  defaultTime = { hour: 13, minute: 30 };
-  meridianTime = { hour: 13, minute: 30 };
-  meridian = true;
+	public editor = ClassicEditor;
+	public editorData = '';
+	defaultTime = { hour: 13, minute: 30 };
+	meridianTime = { hour: 13, minute: 30 };
+	meridian = true;
 
-  SecondsTime: NgbTimeStruct = { hour: 13, minute: 30, second: 30 };
-  seconds = true;
+	SecondsTime: NgbTimeStruct = { hour: 13, minute: 30, second: 30 };
+	seconds = true;
 
-  customTime: NgbTimeStruct = { hour: 13, minute: 30, second: 0 };
-  hourStep = 1;
-  minuteStep = 15;
-  secondStep = 30;
-  eventForm = this.fb.group({
-    event_title: ['', Validators.required],
-    event_short_desc: ['', Validators.required],
-    event_start_date: ['', Validators.required],
-    event_end_date: ['', Validators.required],
-    event_start_time: ['', Validators.required],
-    event_end_time: ['', Validators.required],
-    event_long_desc: [''],
-  });
-  constructor(
-    private fb: FormBuilder,
-    private config: NgbDatepickerConfig,
-    private calendar: NgbCalendar
-  ) {
+	customTime: NgbTimeStruct = { hour: 13, minute: 30, second: 0 };
+	hourStep = 1;
+	minuteStep = 15;
+	secondStep = 30;
+	pageTitle: string;
+	pageAction: string;
+	addEditForm: any;
+	submitted = false;
 
-  }
+	constructor(
+		private fb: FormBuilder,
+		private config: NgbDatepickerConfig,
+		private calendar: NgbCalendar
+	) {
+		this.pageTitle = 'Event';
+		this.pageAction = 'Add';
+		this.addEditForm = this.fb.group({
+			event_title: ['', Validators.required],
+			event_short_desc: ['', Validators.required],
+			event_start_date: ['', Validators.required],
+			event_end_date: ['', Validators.required],
+			event_start_time: ['', Validators.required],
+			event_end_time: ['', Validators.required],
+			event_long_desc: [''],
+		});
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
-  toggleSeconds() {
-    this.seconds = !this.seconds;
-  }
+	toggleSeconds() {
+		this.seconds = !this.seconds;
+	}
 
-  toggleMeridian() {
-    this.meridian = !this.meridian;
-  }
+	toggleMeridian() {
+		this.meridian = !this.meridian;
+	}
 
-  setStartDateConfig() {
-    this.config.minDate = { year: 1900, month: 1, day: 1 };
-    this.config.maxDate = { year: 2099, month: 12, day: 31 };
+	setStartDateConfig() {
+		this.config.minDate = { year: 1900, month: 1, day: 1 };
+		this.config.maxDate = { year: 2099, month: 12, day: 31 };
 
-    // days that don't belong to current month are not visible
-    this.config.outsideDays = 'hidden';
+		// days that don't belong to current month are not visible
+		this.config.outsideDays = 'hidden';
 
-    // weekends are disabled
-    // this.config.markDisabled = (date: NgbDate) => this.calendar.getToday(date) ==;
-  }
+		// weekends are disabled
+		// this.config.markDisabled = (date: NgbDate) => this.calendar.getToday(date) ==;
+	}
 
-  public onSubmit() {
-    console.warn(this.eventForm);
-  }
+	get f(): string {
+		return this.addEditForm.controls;
+	}
 
-  public formSave() {
+	public formSave() {
+		this.submitted = true;
+		console.log(this.addEditForm.value);
+		if (!this.addEditForm.invalid) {
+			console.log(this.addEditForm.value);
 
-  }
+		}
+	}
 
 
 
