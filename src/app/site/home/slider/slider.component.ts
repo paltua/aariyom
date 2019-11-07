4af9e6a518de90d0fb7b281ab54cf4b0df16c755
Ranajit <paltua@gmail.com>
2019-11-07 23:31:43 +0530

    added changes

--- src/app/site/home/slider/slider.component.ts
+++ src/app/site/home/slider/slider.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SiteService } from 'src/app/_service/site.service';
+declare const $: any;
 
 @Component({
   selector: 'app-slider',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slider.component.scss']
 })
 export class SliderComponent implements OnInit {
-
-  constructor() { }
+  list: any;
+  constructor(
+    public siteSer: SiteService
+  ) { }
 
   ngOnInit() {
+    this.siteSer.getEventHomeSlider().subscribe(retData => {
+      this.list = retData.data;
+      // console.log(this.list);
+    })
+  }
+
+  /**
+   * truncate
+   */
+  public truncate(str = '', counter = 0) {
+    return this.siteSer.truncateStr(str, counter);
   }
 
 }
