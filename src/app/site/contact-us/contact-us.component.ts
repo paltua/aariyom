36c77d8872437172f89268359f5822e88d53e5fb
Ranajit <paltua@gmail.com>
2019-11-13 23:20:09 +0530

    added changes

--- src/app/site/contact-us/contact-us.component.ts
+++ src/app/site/contact-us/contact-us.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonService } from 'src/app/_service';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-us.component.scss']
 })
 export class ContactUsComponent implements OnInit {
-
-  constructor() { }
+  email: String;
+  mobile: String;
+  address: String;
+  contactForm: any;
+  constructor(public commonSer: CommonService, private fb: FormBuilder, ) { }
 
   ngOnInit() {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required, Validators],
+      email: ['', [Validators.required, Validators.email]],
+      mobile: ['', Validators.required],
+      desccription: ['', Validators.required],
+    })
+    this.commonSer.getSettings('contact_us').subscribe(retData => {
+      let data: any = retData.data;
+      this.email = data.email;
+      this.mobile = data.mobile;
+      this.address = data.address;
+    })
+  }
+
+  /**
+   * formSave
+   */
+  public formSave() {
+    console.log(this.contactForm);
   }
 
 }
