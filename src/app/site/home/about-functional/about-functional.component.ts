73a953e0b5e56f6b63f69daf8e72880a84cc21ba
Ranajit <paltua@gmail.com>
2019-11-24 00:10:42 +0530

    added changes

--- src/app/site/home/about-functional/about-functional.component.ts
+++ src/app/site/home/about-functional/about-functional.component.ts
@@ -1,65 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { SiteService } from 'src/app/_service/site.service';
-declare const $: any;
+declare let $: any;
 @Component({
-  selector: 'app-about-functional',
-  templateUrl: './about-functional.component.html',
-  styleUrls: ['./about-functional.component.scss']
+	selector: 'app-about-functional',
+	templateUrl: './about-functional.component.html',
+	styleUrls: ['./about-functional.component.scss']
 })
-export class AboutFunctionalComponent implements OnInit {
-  list: any;
-  constructor(
-    public siteSer: SiteService
-  ) { }
+export class AboutFunctionalComponent implements OnInit, AfterViewInit {
+	list: any;
+	listCount: Number = 0;
+	constructor(
+		public siteSer: SiteService
+	) { }
 
-  ngOnInit() {
-    this.list = [];
-    this.getData();
-    setTimeout(() => {
-      this.setCarousel();
-    }, 1000);
+	ngOnInit() {
+		this.list = [];
+		this.getData();
+	}
 
-  }
+	/**
+	 * setCarousel
+	 */
+	public setCarousel() {
+		$('.owl-carousel').owlCarousel({
+			items: this.listCount,
+			margin: 10,
+			loop: true,
+			autoplay: true,
+			nav: true,
+			dots: true,
+			responsive: {
+				0: {
+					items: 1
+				},
+				600: {
+					items: 2
+				},
+				1000: {
+					items: 4
+				}
+			}
+		})
+	}
 
-  /**
-   * setCarousel
-   */
-  public setCarousel() {
-    $('.owl-carousel').owlCarousel({
-      items: 4,
-      margin: 10,
-      autoplay: true,
-      nav: true,
-      dots: true,
-      responsive: {
-        0: {
-          items: 1
-        },
-        600: {
-          items: 2
-        },
-        1000: {
-          items: 4
-        }
-      }
-    })
-  }
+	/**
+	 * getData
+	 */
+	public getData() {
+		this.siteSer.getFunctionalUnitHome().subscribe(retData => {
+			this.list = retData.data;
+			this.listCount = this.list.length + 1;
+			// this.setCarousel();
+		})
+	}
 
-  /**
-   * getData
-   */
-  public getData() {
-    this.siteSer.getFunctionalUnitHome().subscribe(retData => {
-      this.list = retData.data;
-      // this.setCarousel();
-    })
-  }
+	/**
+	 * truncate
+	 */
+	public truncate(str = '', counter = 0) {
+		return this.siteSer.truncateStr(str, counter);
+	}
 
-  /**
-   * truncate
-   */
-  public truncate(str = '', counter = 0) {
-    return this.siteSer.truncateStr(str, counter);
-  }
+	ngAfterViewInit() {
+		setTimeout(() => {
+			this.setCarousel();
+		}, 1000);
+	}
 
 }
